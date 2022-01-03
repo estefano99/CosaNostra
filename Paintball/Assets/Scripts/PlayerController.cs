91aad1e3b1217e9200076ca876ddf32545720c49
estefano99 <estefanobugari@gmail.com>
2022-01-03 19:32:36 -0300

    movimiento + parte de multiplayer

--- Paintball/Assets/Scripts/PlayerController.cs
+++ Paintball/Assets/Scripts/PlayerController.cs
@@ -6,6 +6,7 @@ using UnityEngine;
 public class PlayerController : MonoBehaviour
 {
     public float speed = 5f;
+    public float lookSensitivity = 3f;
 
     private PlayerMotor motor;
 
@@ -25,8 +26,26 @@ public class PlayerController : MonoBehaviour
         Vector3 moveHorizontal = transform.right * xMove;
         Vector3 moveVertical = transform.forward * zMove;
 
+        // Vector final de movimiento
         Vector3 velocity = (moveHorizontal + moveVertical).normalized * speed;
 
+        // Aplicar movimiento
         motor.Move(velocity);
+
+        // Calcular rotacion como vector 3D horizontal
+        float _yRot = Input.GetAxisRaw("Mouse X");
+
+        Vector3 _rotation = new Vector3(0f, _yRot, 0f) * lookSensitivity;
+
+        // Aplicar rotacion
+        motor.Rotate(_rotation);
+
+        // Calcular rotacion como vector 3D vertical
+        float _xRot = Input.GetAxisRaw("Mouse Y");
+
+        Vector3 _cameraRotation = new Vector3(_xRot, 0f, 0f) * lookSensitivity;
+
+        // Aplicar rotacion de camara
+        motor.RotateCamera(_cameraRotation);
     }
 }
